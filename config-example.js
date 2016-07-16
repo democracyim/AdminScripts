f601ea8a0e760fbf3570fea937f15f9a8968c5b4
GaiusSeptimus <liam.gillings+github@gmail.com>
2016-07-16 20:49:02 +0100

    Add in script to remove a test user

---
+++ config-example.js
@@ -0,0 +1,4 @@
+//This needs to be renamed to config.js before running the app
+exports.uri = "mongodb://mongoUser:mongoPass@localhost/databaseName";
+exports.debug = false;
+exports.userEmails = ["test@gmail.com"];
